2d7e0ecb12bb5082306830f24e6879e501b15263
AudreyAAOO <audrey.s.carpentier@gmail.com>
2023-03-06 09:57:10 +0100

    upload ok

--- index.js
+++ index.js
@@ -49,28 +49,41 @@ app.post(
             // Les fichiers reçus sont dans req.files : renvoie un objet ou un tableau si plusieurs images
             // pictures est un tableau d'objets de fichiers, car cette clé contient deux images. 
             // Si une seule image, pictures aurait été un objet de fichiers et non un tableau d'objets
+            console.log("req.files : ", req.files); // afficher les fichiers reçus
 
-            const arrayOfFilesUrl = [];
-            const picturesToUpload = req.files.pictures;
-            
-            for (let i = 0; i < req.files.pictures.length; i++) {
-                const pictures = picturesToUpload[i];
-
+            if (!Array.isArray(req.files.pictures)) {
                 // cloudinary.uploader.upload(file, options). then(callback)
                 const result = await cloudinary.uploader.upload(  // Envoi de l'image à cloudinary
-                    convertToBase64(pictures), // file
-                    {  
+                    convertToBase64(req.files.pictures), // file
+                    {
                         folder: `Vinted/VintedOffers/ImagesOffers/`,// options
-                        // public_id: `img_test`,
+                         public_id: `img_test`
                     }
                 );
-                arrayOfFilesUrl.push(result.secure_url);
+                res.send(result.secure_url);
+            } else {
 
+                const arrayOfFilesUrl = [];
+                const picturesToUpload = req.files.pictures;
+
+                for (let i = 0; i < req.files.pictures.length; i++) {
+                    const pictures = picturesToUpload[i];
+
+                    // cloudinary.uploader.upload(file, options). then(callback)
+                    const result = await cloudinary.uploader.upload(  // Envoi de l'image à cloudinary
+                        convertToBase64(pictures), // file
+                        {
+                            folder: `Vinted/VintedOffers/ImagesOffers/`,// options
+                            // public_id: `img_test`,
+                        }
+                    );
+                    arrayOfFilesUrl.push(result.secure_url);
+                }
+
+                // res.json(arrayOfFilesUrl);
+                res.send(arrayOfFilesUrl);
             }
 
-            console.log("req.files : ", req.files); // afficher les fichiers reçus
-            // res.json(arrayOfFilesUrl);
-            res.send(arrayOfFilesUrl);
             // res.send("OK");
         } catch (error) {
             console.log("catch error: ", error);
@@ -81,32 +94,31 @@ app.post(
 );
 
 //! ***************************************************
-app.delete(
-    "/delete",
-    fileUpload(),
-    async (req, res) => {
-        try {
-            console.log("je suis dans ma route delete");
+// app.delete(
+//     "/delete",
+//     async (req, res) => {
+//         try {
+//             console.log("je suis dans ma route delete");
 
-            // cloudinary.v2.api.delete_resources(public_ids, options).then(callback);
-            const result = await cloudinary.delete_resources(  // suppression des images dans Cloudinary
-                convertToBase64(req.files.pictures), // file
-                {
-                    folder: `Vinted/VintedOffers/ImagesOffers/`,// options
-                    public_id: `${_id}`,
-                }
-            );
-            arrayOfFilesUrl.push(result.secure_url);
+//             // cloudinary.v2.api.delete_resources(public_ids, options).then(callback);
+//             const result = await cloudinary.delete_resources(  // suppression des images dans Cloudinary
+//                 convertToBase64(req.files.pictures), // file
+//                 {
+//                     folder: `Vinted/VintedOffers/ImagesOffers/`,// options
+//                     public_id: `${_id}`,
+//                 }
+//             );
+//             arrayOfFilesUrl.push(result.secure_url);
 
-            res.send("ok");
+//             res.send("ok");
 
-        } catch (error) {
-            console.log("catch error: ", error);
-            console.log("catch error.response: ", error.response);
-            res.status(400).json({ message: error.message });
-        }
-    }
-);
+//         } catch (error) {
+//             console.log("catch error: ", error);
+//             console.log("catch error.response: ", error.response);
+//             res.status(400).json({ message: error.message });
+//         }
+//     }
+// );
 
 app.all("*", (req, res) => {
     res.status(404).json({ message: "⚠️ This route doesn't exist !!! ⚠️" });
